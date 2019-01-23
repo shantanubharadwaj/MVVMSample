61ad3312eaac0e4c6941477309533346b9f3b7d4
Shantanu Dutta <shantanudutta.bharadwaj@gmail.com>
2019-01-23 08:08:50 +0530

    Initial Commit

---
+++ MVVMSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMSample
+//
+//  Created by Shantanu Dutta on 23/01/19.
+//  Copyright © 2019 Shantanu Dutta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
